48eb482a59e9b1348a5b0115980f051ebdd65e65
Pipetr <faaa1901@gmail.com>
2024-10-16 19:59:11 -0400

    first code version finished, testing

---
+++ js/page-color-changer.js
@@ -0,0 +1,16 @@
+
+// Declaring and initializing some constants for the input slider elements
+const redSlider = document.querySelector('#red');
+const greenSlider = document.querySelector('#green');
+const blueSlider = document.querySelector('#blue');
+const pageBody = document.querySelector('body');
+
+// function to change the background color of the page based on the values of the sliders
+function changeBackgroundColor() {
+    pageBody.style.backgroundColor = `rgb(${redSlider.value}, ${greenSlider.value}, ${blueSlider.value})`;
+}
+
+// Adding event listeners to the sliders to listen for input events
+redSlider.addEventListener('input', changeBackgroundColor);
+greenSlider.addEventListener('input', changeBackgroundColor);
+blueSlider.addEventListener('input', changeBackgroundColor);
