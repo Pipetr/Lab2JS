b0cc0a8de9aad0a252c578dd70350f430deb4748
Pipetr <faaa1901@gmail.com>
2024-10-16 20:17:09 -0400

    added a new slider, to increase text size

--- js/page-color-changer.js
+++ js/page-color-changer.js
@@ -4,13 +4,20 @@ const redSlider = document.querySelector('#red');
 const greenSlider = document.querySelector('#green');
 const blueSlider = document.querySelector('#blue');
 const pageBody = document.querySelector('body');
+const fontSizeSlider = document.querySelector('#font-size');
+const paragraph = document.querySelector('p');
 
 // function to change the background color of the page based on the values of the sliders
 function changeBackgroundColor() {
     pageBody.style.backgroundColor = `rgb(${redSlider.value}, ${greenSlider.value}, ${blueSlider.value})`;
 }
 
+function changeFontSize() {
+    paragraph.style.fontSize = `${fontSizeSlider.value}px`;
+}
+
 // Adding event listeners to the sliders to listen for input events
 redSlider.addEventListener('input', changeBackgroundColor);
 greenSlider.addEventListener('input', changeBackgroundColor);
 blueSlider.addEventListener('input', changeBackgroundColor);
+fontSizeSlider.addEventListener('input', changeFontSize);
