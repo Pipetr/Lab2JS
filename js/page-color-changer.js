38bd6b9417a132b3102aef53093737ea453e3fee
Pipetr <faaa1901@gmail.com>
2024-10-16 20:21:38 -0400

    changed font color depending on the background color

--- js/page-color-changer.js
+++ js/page-color-changer.js
@@ -6,10 +6,27 @@ const blueSlider = document.querySelector('#blue');
 const pageBody = document.querySelector('body');
 const fontSizeSlider = document.querySelector('#font-size');
 const paragraph = document.querySelector('p');
+const h1 = document.querySelector('h1');
+const labels = document.querySelectorAll('label');
 
 // function to change the background color of the page based on the values of the sliders
 function changeBackgroundColor() {
     pageBody.style.backgroundColor = `rgb(${redSlider.value}, ${greenSlider.value}, ${blueSlider.value})`;
+    // if the background color is too bright, change the text color to black
+    if (parseInt(redSlider.value) + parseInt(greenSlider.value) + parseInt(blueSlider.value) > 382) {
+        paragraph.style.color = 'black';
+        h1.style.color = 'black';
+        labels.forEach(label => {
+            label.style.color = 'black';
+        });
+
+    } else {
+        paragraph.style.color = 'white';
+        h1.style.color = 'white';
+        labels.forEach(label => {
+            label.style.color = 'white';
+        });
+    }
 }
 
 function changeFontSize() {
