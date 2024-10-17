0ac01f26684507b0ca937a5744f2a2e860e1f11d
Pipetr <faaa1901@gmail.com>
2024-10-16 20:25:55 -0400

    modified the maximums for font size changing, added some comments

--- js/page-color-changer.js
+++ js/page-color-changer.js
@@ -13,22 +13,22 @@ const labels = document.querySelectorAll('label');
 function changeBackgroundColor() {
     pageBody.style.backgroundColor = `rgb(${redSlider.value}, ${greenSlider.value}, ${blueSlider.value})`;
     // if the background color is too bright, change the text color to black
-    if (parseInt(redSlider.value) + parseInt(greenSlider.value) + parseInt(blueSlider.value) > 382) {
+    if (parseInt(redSlider.value) + parseInt(greenSlider.value) + parseInt(blueSlider.value) > 500) {
         paragraph.style.color = 'black';
         h1.style.color = 'black';
-        labels.forEach(label => {
+        labels.forEach(label => { // change the color of the labels to black
             label.style.color = 'black';
         });
 
     } else {
         paragraph.style.color = 'white';
         h1.style.color = 'white';
-        labels.forEach(label => {
+        labels.forEach(label => { // change the color of the labels to white.
             label.style.color = 'white';
         });
     }
 }
-
+// function to change the font size of the paragraph based on the value of the font size slider
 function changeFontSize() {
     paragraph.style.fontSize = `${fontSizeSlider.value}px`;
 }
